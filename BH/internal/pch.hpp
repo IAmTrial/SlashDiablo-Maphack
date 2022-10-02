f05c9d8c2c5d5c8ea6c661ea85719fdf7f5e219c
Mir Drualga <IAmRealTrial+SlashBH@gmail.com>
2022-10-02 13:23:44 -0700

    Add precompile headers

---
+++ BH/internal/pch.hpp
@@ -0,0 +1,59 @@
+/**
+ * SlashDiablo Maphack
+ * Copyright (C) SlashDiablo Community
+ *
+ * This file is part of SlashDiablo Maphack.
+ *
+ * SlashDiablo Maphack is free software: you can redistribute it
+ * and/or modify it under the terms of the GNU Affero General Public
+ * License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef BH_INTERNAL_PCH_HPP_
+#define BH_INTERNAL_PCH_HPP_
+
+#include <algorithm>
+#include <array>
+#include <atomic>
+#include <chrono>
+#include <deque>
+#include <filesystem>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iostream>
+#include <limits>
+#include <list>
+#include <map>
+#include <memory>
+#include <numeric>
+#include <optional>
+#include <ostream>
+#include <regex>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <string>
+#include <string_view>
+#include <thread>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+
+#include "pch.h"
+
+#endif  // BH_INTERNAL_PCH_HPP_
