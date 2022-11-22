da06de46734f720dcc2ecc86d36de64ac2ea1ccb
Mir Drualga <IAmRealTrial+SlashBH@gmail.com>
2022-11-21 20:53:52 -0800

    Add MpqHandle struct aliases for other versions

---
+++ src/bh/d2/struct/v1_10/mpq_handle.hpp
@@ -0,0 +1,33 @@
+/**
+ * SlashDiablo Maphack
+ * Copyright (C) 2012-2022  SlashDiablo Team
+ *
+ * This file is part of SlashDiablo Maphack.
+ *
+ * SlashDiablo Maphack is free software: you can redistribute it
+ * and/or modify it under the terms of the GNU Affero General Public
+ * License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef BH_D2_STRUCT_V1_10_MPQ_HANDLE_HPP_
+#define BH_D2_STRUCT_V1_10_MPQ_HANDLE_HPP_
+
+#include "bh/d2/struct/v1_00/mpq_handle.hpp"
+
+namespace bh::d2::v1_10 {
+
+using MpqHandle = ::bh::d2::v1_00::MpqHandle;
+
+}  // namespace bh::d2::v1_10
+
+#endif  // BH_D2_STRUCT_V1_10_MPQ_HANDLE_HPP_
