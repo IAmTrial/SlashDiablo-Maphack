e4f62fa5a2bbb3250a593a1f988016eb9aae4516
Mir Drualga <IAmRealTrial+SlashBH@gmail.com>
2023-03-12 00:55:37 -0800

    Implement Windows HANDLE closer
    This solves a Windows HANDLE program sturcture issue by using RAII.

---
+++ src/bh/common/windows/handle_closer.hpp
@@ -0,0 +1,36 @@
+/**
+ * SlashDiablo Maphack
+ * Copyright (C) 2012-2022  SlashDiablo Team
+ *
+ * This file is part of SlashDiablo Maphack.
+ *
+ * SlashDiablo Maphack is free software: you can redistribute it
+ * and/or modify it under the terms of the GNU Affero General Public
+ * License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+
+#include <windows.h>
+
+namespace bh::common::windows {
+
+class HandleCloser {
+ public:
+  explicit constexpr HandleCloser(HANDLE handle) : handle_(handle) {}
+
+  ~HandleCloser();
+
+ private:
+  HANDLE handle_;
+};
+
+}  // namespace bh::common::windows
