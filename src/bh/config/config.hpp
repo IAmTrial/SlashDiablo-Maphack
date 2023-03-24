579a058ba17aa0de559a2e78e01c8905d6ceeb88
Mir Drualga <IAmRealTrial+SlashBH@gmail.com>
2023-03-24 01:30:25 -0700

    Add in-progress config file

---
+++ src/bh/config/config.hpp
@@ -0,0 +1,39 @@
+/**
+ * SlashDiablo Maphack
+ * Copyright (C) 2012-2022  SlashDiablo Team
+ *
+ * This file is part of SlashDiablo Maphack.
+ *
+ * SlashDiablo Maphack is free software: you can redistribute it
+ * and/or modify it under the terms of the GNU Affero General Public
+ * License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * BH
+ * Copyright (C) 2011  McGod
+ *
+ * All rights reserved.
+ */
+
+#ifndef BH_CONFIG_CONFIG_HPP_
+#define BH_CONFIG_CONFIG_HPP_
+
+namespace bh::config {
+
+struct Config {
+};
+
+}  // namespace bh::config
+
+#endif  // BH_CONFIG_CONFIG_HPP_
